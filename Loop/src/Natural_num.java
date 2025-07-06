7b674914a0cb7fcac1c629ec56ac66c725a3750f
anushika0609 <anushkasaini0609@gmail.com>
2025-07-06 12:32:15 +0530

    add new file

---
+++ Loop/src/Natural_num.java
@@ -0,0 +1,10 @@
+public class Natural_num {
+    public static void main(String[] args) {
+        System.out.println("Natural numbers between 100 and 200 are -->");
+        int i =100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
